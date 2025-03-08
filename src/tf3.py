e7b9861b688de183b5a3137db74ab9363ec9ca15
Hasnain081992 <Hasnain081992@gmail.com>
2025-03-08 10:17:57 +0000

    ok

--- src/tf3.py
+++ src/tf3.py
@@ -3,10 +3,10 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("Minipro").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "tfl_underground_pyspark").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "demo_hasan").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
-df.write.mode("overwrite").saveAsTable("lokhandwala.tfl_underground_pyspark")
+df.write.mode("overwrite").saveAsTable("lokhandwala.demohas")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/full_load_postgresToHive.py
