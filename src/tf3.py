7285882fcfb98ba7a79c988e63fcd99a1b2d3456
Hasnain081992 <Hasnain081992@gmail.com>
2025-03-08 00:53:43 +0000

    ok

--- src/tf3.py
+++ src/tf3.py
@@ -6,7 +6,7 @@ spark = SparkSession.builder.master("local").appName("tflProject").enableHiveSup
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "tfl_underground_pyspark").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
-df.write.mode("overwrite").saveAsTable("default.tfl_data2")
+df.write.mode("overwrite").saveAsTable("default.tfl_data3")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/full_load_postgresToHive.py
