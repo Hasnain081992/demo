6eb6cef537e1f9709616f2eb096e1a3cb5bb505b
Hasnain081992 <Hasnain081992@gmail.com>
2025-03-08 21:32:15 +0000

    ok

--- src/tf3.py
+++ src/tf3.py
@@ -6,7 +6,13 @@ spark = SparkSession.builder.master("local").appName("Minipro").enableHiveSuppor
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "tfl_underground_pyspark").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
-df.write.mode("overwrite").saveAsTable("lokhandwala.tfldata")
-print("Successfully Load to Hive")
+# Transformation 1: Convert 'Timestamp' column to timestamp data type
+df_transformed = df.withColumn("Timestamp", to_timestamp(col("Timestamp"), "dd/MM/yyyy HH:mm"))
 
+# Transformation 2: Replace 'N/A' with None
+df_transformed = df_transformed.na.replace("N/A", None)
+
+# Write the transformed DataFrame to Hive table
+df_transformed.write.mode("overwrite").saveAsTable("lokhandwala.tfl")
+print("Successfully loaded to Hive")
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/full_load_postgresToHive.py
